89ac526d90f740e2f714b6effc0c7456bbe9da9e
Gunkawee <610107030018@dpu.ac.th>
2024-04-02 20:34:32 +0700

    update

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
     selector: 'app-news',
     standalone: true,
     templateUrl: './news.component.html',
     styleUrl: './news.component.css',
-    imports: [FooterComponent]
+    imports: [FooterComponent, HeaderComponent]
 })
 export class NewsComponent {
 
